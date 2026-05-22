0592f3f08bc575108dedd3f2118d89f82762fef9
Tarunna Sen <tarunnasen@gmail.com>
2026-05-22 17:55:35 +0600

    Removed JWT

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const { jwtVerify, createRemoteJWKSet } = require("jose-cjs");
 
 dotenv.config();
 
@@ -28,29 +27,6 @@ const client = new MongoClient(uri, {
 });
 
 let collection;
-
-const JWKS = createRemoteJWKSet(
-  new URL(`process.env.CLIENT_URL${"/api/auth/jwks"}`),
-);
-
-const verifyJWT = async (req, res, next) => {
-  const header = req?.headers.authorization;
-  if (!header) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  const token = header.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  try {
-    const { payload } = await jwtVerify(token, JWKS);
-    next();
-  } catch (error) {
-    return res.status(403).json({ error: "Forbidden" });
-  }
-};
 
 async function run() {
   try {
@@ -83,7 +59,7 @@ async function run() {
       res.json(trendingIdeas);
     });
 
-    app.get("/ideas/:id", verifyJWT, async (req, res) => {
+    app.get("/ideas/:id", async (req, res) => {
       const { id } = req.params;
       const idea = await collection.findOne({ _id: new ObjectId(id) });
       res.json(idea);
